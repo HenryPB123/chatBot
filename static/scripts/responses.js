fd0a41f2704f7067d06382ba40313876869a1f67
Henry P B <mahe2di@gmail.com>
2023-06-07 21:37:28 -0500

    res1

--- static/scripts/responses.js
+++ static/scripts/responses.js
@@ -5,7 +5,9 @@ var morningGreet = RegExp("(BUEN DIA|BUEN DÍA|BUENOS DIAS|BUENOS DÍAS)");
 var afternoonGreet = RegExp("(BUENA TARDE|BUENAS TARDES)");
 var eveningGreet = RegExp("(BUENA NOCHE|BUENAS NOCHES)");
 
-var what = RegExp("(QUE|QUÉ|CUAL|CUÁL|CUALES|SABER|CONOCER)");
+var what = RegExp(
+  "(QUE|QUÉ|CUAL|CUÁL|CUALES|SABER|CONOCER|ENVIA|ENVÍA|ENVIAS|ENVÍAS|DA|PODRIA|PODRÍA|PODRÍAS|PODRIAS)"
+);
 var carta = RegExp(
   "(MENU|MENÚ|CARTA|TIPO|TIPOS|CLASSES|CLASE|SABOR|SABORES|PIZZA|PIZZAS|PIZA|PIZAS)"
 );
@@ -14,6 +16,9 @@ var toWant = RegExp(
   "(MUESTRA|MUESTRAS|PUEDES|QUIERO|DESEO|VOY|QUISIERA|GUSTARIA|GUSTARÍA|DAME|DEME)"
 );
 var toHave = RegExp("(TIENE|TIENES|TIENEN|OFRECE|DISPONE)");
+var flavorsPizza = RegExp(
+  "(POLLO|CHAMPIÑONES|PEPERONI|PEPERONNI|HAWAIANA|HAWAIANNA)"
+);
 var flavorChiken = RegExp("(POLLO|CHAMPIÑONES)");
 var flavorPeperonni = RegExp("(PEPERONI|PEPERONNI)");
 var flavorHawaianna = RegExp("(HAWAIANA|HAWAIANNA)");
@@ -40,26 +45,18 @@ function menu() {
   return `El día de hoy ofrecemos los siguiéntes sabores:\n <b>Peperonni,</b>\n<b>Pollo con champiñones y </b>\n<b>Hawaianna</b>\n¿Cúal le gustaría?`;
 }
 
-// function flavorsPizza(flavor) {
-//   return `Sabor a  ${flavor} tiene tres tamaños disponibles: <ul><li>Pequeña (4 Porciones) cuesta 160 pesos</li><li>Mediana (8 Porciones) cuesta 220 pesos</li><li>Familiar (12 Porciones) cuesta 330 pesos</li></ul>`;
-// }
-
-// function sizesPizza(size) {
-//   size = size.toUpperCase();
-//   if (size.includes("PEQUEÑA"))
-//     return "La pequeña tiene un costo de 160 pesos, tiene 4 porfciones y tarda de 15 a 20 minutos su preparación.";
-//   else if (size.includes("MEDIANA"))
-//     return "La mediana tiene un costo de 220 pesos, tiene 8 porfciones y tarda de 15 a 20 minutos su preparación.";
-//   else if (size.includes("GRANDE"))
-//     return "La grande tiene un costo de 330 pesos, tiene 8 porfciones y tarda de 15 a 20 minutos su preparación.";
-//   else return `Este tamaño no está disponible`;
-// }
+function flavors(input) {
+  console.log("input", input);
+  return "sabores";
+}
 
 function getBotResponse(input) {
   input = input.toUpperCase();
   if (input === "") return "No has escrito nada, ¿Qué te gustaría saber?";
   else if (saludo.test(input) == true) return toGreet(input);
   else if (what.test(input) == true && carta.test(input) == true) return menu();
+  else if (toWant.test(input) == true && flavorsPizza.test(input) == true)
+    return flavors(input);
   // else if (
   //   (toWant.test(input) == true || what.test(input) == true) &&
   //   menu.test(input) == true
